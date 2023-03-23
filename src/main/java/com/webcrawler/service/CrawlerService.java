9e7e6ae10ab75e0e8764bc2c8aebea92993fb0a2
lillakiraly <kdotlilla@gmail.com>
2023-03-23 16:26:12 +0100

    Incorporate use of dto and communication with controller layer

--- src/main/java/com/webcrawler/service/CrawlerService.java
+++ src/main/java/com/webcrawler/service/CrawlerService.java
@@ -1,5 +1,6 @@
 package com.webcrawler.service;
 
+import com.webcrawler.dto.CrawlerRequestDto;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -7,24 +8,20 @@ import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Set;
 
 @Service
 public class CrawlerService {
     private static final int DEFAULT_DEPTH = 1;
 
-    private Set<String> visitedLinks;
-
-    public CrawlerService(Set<String> visitedLinks) {
-        this.visitedLinks = visitedLinks;
+    public Set<String> crawl(CrawlerRequestDto crawlerRequestDto) {
+        Set<String> visitedLinks = new HashSet<>();
+        crawlPage(crawlerRequestDto.getUrl(), crawlerRequestDto.getDepth(), visitedLinks);
+        return visitedLinks;
     }
 
-    public Document connectToPage(String url) throws IOException {
-        return Jsoup.connect(url).get();
-    }
-
-
-    public void crawlPage(String url, int depth) {
+    private void crawlPage(String url, int depth, Set<String> visitedLinks) {
         if (!visitedLinks.contains(url) && 0 <= depth
                 //cannot handle href="javascript:(some function)"
                 && (url.contains("http") || url.contains("https"))) {
@@ -35,11 +32,15 @@ public class CrawlerService {
                 Elements linksOnDocument = document.select("a[href]");
                 for (Element element : linksOnDocument) {
                     String link = element.attr("abs:href");
-                    crawlPage(link, depth - 1);
+                    crawlPage(link, depth - 1, visitedLinks);
                 }
             } catch (IOException ioException) {
                 System.out.println(ioException);
             }
         }
+    }
+
+    private Document connectToPage(String url) throws IOException {
+        return Jsoup.connect(url).get();
     }
 }
