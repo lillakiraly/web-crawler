5c31d807e2351109479d825d4d6121aa02cce00d
lillakiraly <kdotlilla@gmail.com>
2023-03-23 16:27:27 +0100

    Implement get response

--- src/main/java/com/webcrawler/controller/CrawlerController.java
+++ src/main/java/com/webcrawler/controller/CrawlerController.java
@@ -1,4 +1,26 @@
 package com.webcrawler.controller;
 
+import com.webcrawler.dto.CrawlerRequestDto;
+import com.webcrawler.service.CrawlerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+
+@RestController
+@RequestMapping("/")
 public class CrawlerController {
+    private CrawlerService crawlerService;
+
+    @Autowired
+    public CrawlerController(CrawlerService crawlerService) {
+        this.crawlerService = crawlerService;
+    }
+
+    @GetMapping
+    public Set<String> getCrawlResults(@RequestBody CrawlerRequestDto req) {
+        Set<String> collectedUrls = crawlerService.crawl(req);
+        return collectedUrls;
+    }
+
 }
