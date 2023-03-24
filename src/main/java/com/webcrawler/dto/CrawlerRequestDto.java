d274c24104becdf1d1343b95e3771f388abd1c26
lillakiraly <kdotlilla@gmail.com>
2023-03-24 13:16:25 +0100

    Add toString

--- src/main/java/com/webcrawler/dto/CrawlerRequestDto.java
+++ src/main/java/com/webcrawler/dto/CrawlerRequestDto.java
@@ -2,9 +2,11 @@ package com.webcrawler.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class CrawlerRequestDto {
     private String url;
     private int depth;
